b4a221c6da84626e1fc2624a2f20da9ad9f8bd93
manan228 <manan228@yahoo.in>
2022-06-17 21:24:08 +0530

    Taks 4 complete, added background color to 3 item in list, changed font to bold for all listGroup

--- Task3/dom.js
+++ Task3/dom.js
@@ -4,3 +4,12 @@ const addTitle = document.getElementsByClassName(`title`);
 headerTitle.style.borderBottom = `solid 3px #000`;
 
 addTitle[0].style.color = `green`;
+
+const listGroup = document.getElementsByClassName(`list-group-item`);
+
+listGroup[2].style.backgroundColor = `green`;
+
+for(let i=0;i<listGroup.length;i++) {
+
+    listGroup[i].style.fontWeight = `bold`;
+}
