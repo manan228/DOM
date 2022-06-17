472ff25fad770d32dcb76b78a68a46eb46392a5b
manan228 <manan228@yahoo.in>
2022-06-17 20:53:35 +0530

    task 3 completed

---
+++ Task3/dom.js
@@ -0,0 +1,6 @@
+const headerTitle = document.getElementById(`header-title`);
+const addTitle = document.getElementsByClassName(`title`);
+
+headerTitle.style.borderBottom = `solid 3px #000`;
+
+addTitle[0].style.color = `green`;
